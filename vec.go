63018ea53dc486a44aa2dedded3bc06b01d9de8b
Sean <4583759+sean-brock@users.noreply.github.com>
2019-03-22 18:54:49 -0500

    Added vec.go

---
+++ vec.go
@@ -0,0 +1,23 @@
+package dimension
+
+import "fmt"
+
+// Vec is a container of 2 ints, X and Y (a 2D integer vector).
+type Vec struct {
+	X, Y int
+}
+
+// V returns a new Vec.
+func V(x, y int) Vec {
+	return Vec{X: x, Y: y}
+}
+
+// Add returns the sum of the vectors.
+func (u Vec) Add(v Vec) Vec {
+	return V(u.X+v.X, u.Y+v.Y)
+}
+
+// String returns the string representation of the vector.
+func (u Vec) String() string {
+	return fmt.Sprintf("Vec(%v, %v)", u.X, u.Y)
+}
